adf3cc382250be9bd094549e2c729c06e8216cc6
Nadeem <mnadeem@contractor.solenis.com>
2026-06-02 19:56:05 +0530

    Add Agent1 and Framework projects; implement AI chat functionality

---
+++ Random1/Program.cs
@@ -0,0 +1,14 @@
+﻿using DotNetEnv;
+using Framework;
+using Microsoft.Agents.AI;
+using OpenAI;
+
+using var timer = new CodeTimer("Load Env");
+Env.TraversePath().Load();
+
+var audioClient = AI.OpenRouter.GetAudioClient("google/lyria-3-pro-preview");
+// await audioClient.CreateVoiceAsync(new BinaryCo)
+
+OpenAIClient client = new OpenAIClient()
+
+// Console.WriteLine(Env.GetString("MODEL"));
